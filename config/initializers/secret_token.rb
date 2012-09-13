a7235dbddfe63f95d6b68eee79615507d9d196b4
Edis Omerovic <edis.omerovic@p3rsonal.com>
2012-09-13 13:41:06 -0700

    First commit, adding initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsworkout::Application.config.secret_token = 'c5ceaf1a23c37897a2387dba629fb16820c740e5f6831658bf2ee91bae32f3b88f16872e8802ac3b2c69437a20870b75724cf167033994b9a378ca105291b19b'
