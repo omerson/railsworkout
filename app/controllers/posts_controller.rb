380828836b65b1d6dce7d82ef0053c363e039b65
Edis Omerovic <edis.omerovic@p3rsonal.com>
2012-09-17 14:25:10 -0700

    CRUD basic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,27 +4,41 @@ class PostsController < ApplicationController
   end
   
   def show
-      
+      @post = Post.find(params[:id])
   end
   
   def new
-          
+       @post = Post.new   
   end
    
   def create
-          
+      @post = Post.new(params[:post])
+      
+      if @post.save
+        redirect_to posts_path, :notice => "Your post is saved"
+      else
+        render "new"        
+      end
   end
   
   def edit
-    
+    @post = Post.find(params[:id])
   end
   
   def update
+    @post = Post.find(params[:id])
     
+    if @post.update_attributes(params[:post])
+      redirect_to posts_path, :notice => "Your post has been updated"      
+    else
+      render "edit"
+    end
   end
   
   def destroy
-    
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, :notice => "Your post has been deleted" 
   end
   
   
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :title
+  
+  validates :content, :title, :presence => true
+  validates :title, :length => { :minimum => 2 }
+  validates :title, :uniqueness => { :message => "already taken" }
+  
 end
